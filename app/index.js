b011057dcce3a697197d565f93e102c104fb8784
JM <mue.navarone@gmail.com>
2012-11-12 22:28:09 -0600

    Edoku app.

---
+++ app/index.js
@@ -0,0 +1,11 @@
+/*
+ * Edoku, another NodeJS e-learnig CMS.
+ *
+ * This files is the core of application.
+ * Usage: node app, or NODE_ENV=production node app
+ *
+ * jmrey
+ * 
+ */
+var edk = require('./../edk');
+edk.run();
